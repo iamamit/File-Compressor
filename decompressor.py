9e58d124c4e4618d7a0152f23d5e983539ecf1df
Amit Gautam <amit99.ag@gmail.com>
2021-07-09 23:38:11 +0530

    Little optimization

--- decompressor.py
+++ decompressor.py
@@ -14,9 +14,9 @@ class Decompressor:
             bit_string = self.__bitString(file)
             actual_bit_string = self.__removePadding(bit_string)
 
-            actual_string = self.__convert_actual_string(actual_bit_string)
+            actual_string = self.__convert_actual_string(actual_bit_string,output)
             
-            output.write(actual_string)
+            # output.write(actual_string)
             
         pass
 
@@ -47,19 +47,32 @@ class Decompressor:
         
         pass
 
-    def __convert_actual_string(self,actual_bit_string):
+    def __convert_actual_string(self,actual_bit_string,output):
         # Initialize progress bar
         actual_bit_string_length = len(actual_bit_string)
         p = ProgressBar(actual_bit_string_length)
 
         actual_string = ""
+        actual_string_list = []
+        start = 0
+        end = 1
         # print(self.compressor.reverse_charbit_map)
-        bit_string = ''
+        # bit_string = []
         for i in p.progress:
-            bit_string += str(actual_bit_string[i])
-            if bit_string in self.compressor.reverse_charbit_map:
-                actual_string += self.compressor.reverse_charbit_map[bit_string]
-                bit_string = ''
+            # bit_string.append(str(actual_bit_string[i]))
 
+            # if ''.join(bit_string) in self.compressor.reverse_charbit_map:
+            #     actual_string_list.append(self.compressor.reverse_charbit_map[''.join(bit_string)])
+            #     bit_string = []
+
+            if actual_bit_string[start:end] in self.compressor.reverse_charbit_map:
+                # actual_string_list.append(self.compressor.reverse_charbit_map[actual_bit_string[start:end]])
+                output.write(self.compressor.reverse_charbit_map[actual_bit_string[start:end]])
+                start = end
+                end = end + 1
+            else:
+                end = end + 1
+
+        actual_string = ''.join(actual_string_list)
         return(actual_string)
         pass
